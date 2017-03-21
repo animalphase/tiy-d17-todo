b4c9ffffbb5816ce8007b1d8e7df8c92e400b960
charli elwonger <celwonger@gmail.com>
2017-03-21 17:19:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Etsy Search Page
+
+## TIY Week 3, day 16
+
+**⭐️ live on surge: http://effie-search.surge.sh/ ⭐️**
+
+----
+
+### Libraries/tools used ⚒
+
+- jQuery
+- Sass
+- Bourbon
+- Neat
+
+#### Assignment
+
+https://newline.theironyard.com/paths/938/units/5392/assignments/13409
+
+#### Objectives
+- Create a function that makes an AJAX request to the Etsy API using whatever search term you'd like.
+- Create a data handler function to handle the data when it (asynchronously) returns from the API. Use this data to populate your webpage!
+- Recreate the Etsy search results page, styled as closely as possible to the actual Etsy site..
+
+#### Extra Objectives
+- Allow the user to change the search term. When they make a new search, refetch the data and update the results list
+- Pull the categories from the data to create the sidebar, with links that filter the listings.
+- Implement the sorting dropdown.
---
+++ main.js
@@ -0,0 +1,130 @@
+
+
+
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Global Config
++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+var $g_taskUl = $('#task-list');
+var $g_taskInput = $('#task-input');
+var $g_taskSubmitButton = $('#task-submit-button');
+var $g_clearTasksLink = $('.clear-tasks-link');
+
+var g_rootURL = 'http://tiny-za-server.herokuapp.com/collections/moon-todo/';
+
+var g_getTaskSettings = {
+  type: 'GET',
+  dataType: 'json',
+  url: g_rootURL
+};
+
+var exampleTask = {
+  "task": "example task",
+  "complete": false
+};
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    App behavior managed by event handlers
++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+$g_taskSubmitButton.on('click', addTask);
+$g_clearTasksLink.on('click', clearAllTasks);
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Functions
++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+function addTask() {
+  var inputText = $g_taskInput.val();
+  if(inputText === '' || inputText === undefined) {
+    console.log('no task input');
+    return false;
+  }
+  $g_taskInput.val('');
+  var newTask = {
+    task: inputText,
+    complete: false
+  };
+  var postSettings = {
+    type: 'POST',
+    contentType: 'application/json',
+    url: g_rootURL,
+    data: JSON.stringify(newTask)
+  };
+  $.ajax(postSettings).then(getTasks);
+  return false;
+}
+
+
+function getTasks() {
+  $.ajax(g_getTaskSettings).then(renderTasks);
+}
+
+
+function renderTasks(data, status, xhr) {
+  console.log(data);
+  $g_taskUl.html('');
+  data.forEach(function(task, i, array){
+    var $taskItem = $('<li>');
+    $taskItem.html(task.task);
+
+    var $deleteTaskButton = $('<button class="delete-task-button">X</button>');
+    $taskItem.append($deleteTaskButton);
+
+    var $completeTaskButton = $('<button class="complete-task-button">☐</button>');
+    if (task.complete === true){
+      $completeTaskButton.html('☑︎');
+    } else {
+      $completeTaskButton.html('☐');
+    }
+    $taskItem.prepend($completeTaskButton);
+
+    $g_taskUl.append($taskItem);
+
+    $deleteTaskButton.on('click', function(){deleteTask(task);});
+    $completeTaskButton.on('click', function(){completeTask(task);});
+  });
+}
+
+
+function deleteTask(task) {
+  var id = task._id;
+  var url = g_rootURL + id;
+  var deleteSettings = {
+    type: 'DELETE',
+    url: url
+  };
+  $.ajax(deleteSettings).then(getTasks);
+}
+
+function clearAllTasks() {
+  $.ajax(g_getTaskSettings).then(function(data, status, xhr){
+    data.forEach(function(task){
+      deleteTask(task);
+    });
+  });
+}
+
+
+function completeTask(task) {
+  var id = task._id;
+  var url = g_rootURL + id;
+  task.complete = !task.complete;
+
+  var putSettings = {
+    type: 'PUT',
+    url: url
+  };
+
+  $.ajax(deleteSettings).then(getTasks);
+}
+
+
+// do the jQuery
+$(function () {
+  getTasks();
+});
